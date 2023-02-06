efe8f5c4fb1685e9fe7235f035a2ad6ce7f3a3e8
Jongheon Eom <e4033jh@daum.net>
2023-02-06 09:15:26 +0900

    2023-02-06 commit

---
+++ src/java_/_03_static/CommonUtils.java
@@ -0,0 +1,20 @@
+package java_._03_static;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class CommonUtils {
+
+    public static LocalDateTime stringToLocalDateTime(String strDate) {
+        return LocalDateTime.parse(strDate, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
+
+    public static String localDateTimeToString(LocalDateTime localDateTime) {
+        return localDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
+
+//    public static boolean isEmailValid(String email) {
+//        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
+//    }
+
+}
---
+++ src/java_/_03_static/Test.java
@@ -0,0 +1,13 @@
+package java_._03_static;
+
+public class Test {
+
+    public static void sm() {
+        System.out.println("this is static method");
+    }
+
+    public void m() {
+        System.out.println("this is non-static method");
+    }
+
+}
