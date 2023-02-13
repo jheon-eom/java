1f61991783f420d916636841842c799e7663c7a9
Jongheon Eom <e4033jh@daum.net>
2023-02-13 22:49:25 +0900

    2023-02-13 Generic

---
+++ src/generic_/ExGeneric.java
@@ -0,0 +1,8 @@
+package generic_;
+
+public class ExGeneric implements ExInterfaceGeneric {
+    @Override
+    public String example() {
+        return null;
+    }
+}
---
+++ src/generic_/ExInterfaceGeneric.java
@@ -0,0 +1,5 @@
+package generic_;
+
+public interface ExInterfaceGeneric<T> {
+    T example();
+}
