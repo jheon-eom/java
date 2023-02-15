82512fe99407de07f25b9a36d7467511514571e3
jheon-eom <e4033jh@daum.net>
2023-02-15 18:37:56 +0900

    2023-02-15 item7 다 쓴 객체 참조를 해제하라

---
+++ src/effective/ch02/item07/ex/Stack.java
@@ -0,0 +1,35 @@
+package effective.ch02.item07.ex;
+
+import java.util.Arrays;
+import java.util.EmptyStackException;
+
+public class Stack {
+    private Object[] elements;
+    private int size;
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+
+    public Stack() {
+        elements = new Object[DEFAULT_INITIAL_CAPACITY];
+    }
+
+    public void push(Object e) {
+        ensureCapacity();
+        elements[size++] = e;
+    }
+
+    public Object pop() {
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+        size--;
+        Object element = elements[size];
+        elements[size] = null;
+        return element;
+    }
+
+    private void ensureCapacity() {
+        if (elements.length == size) {
+            elements = Arrays.copyOf(elements, 2 * size + 1);
+        }
+    }
+}
---
+++ src/effective/ch02/item07/ex/UseStack.java
@@ -0,0 +1,14 @@
+package effective.ch02.item07.ex;
+
+public class UseStack {
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+
+        for (int i = 0; i < 10; i++) {
+            Object SomeObj = new Object();
+            stack.push(SomeObj);
+        }
+        java.util.Stack
+        Object pop = stack.pop();
+    }
+}
