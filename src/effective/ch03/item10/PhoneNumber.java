62a0565308fb8fa1e8c421a83ae564b3886e739a
Jongheon Eom <e4033jh@daum.net>
2023-02-21 21:12:31 +0900

    equals 재정의

---
+++ src/effective/ch03/item10/PhoneNumber.java
@@ -0,0 +1,33 @@
+package effective.ch03.item10;
+
+public final class PhoneNumber {
+    private final short areaCode;
+    private final short prefix;
+    private final short lineNum;
+
+    public PhoneNumber(int areaCode, int prefix, int lineNum) {
+        this.areaCode = rangeCheck(areaCode, 999, "지역코드");
+        this.prefix = rangeCheck(prefix, 999, "프리픽스");
+        this.lineNum = rangeCheck(lineNum, 9999, "가입자 번호");
+    }
+
+    private static short rangeCheck(int val, int max, String arg) {
+        if (val < 0 || val > max) {
+            throw new IllegalArgumentException(arg + ": " + val);
+        }
+        return (short) val;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof PhoneNumber)) {
+            return false;
+        }
+        PhoneNumber pn = (PhoneNumber) obj;
+        return pn.lineNum == this.lineNum && pn.areaCode == this.areaCode
+                && pn.prefix == this.prefix;
+    }
+}
