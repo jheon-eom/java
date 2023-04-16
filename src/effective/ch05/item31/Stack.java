476e70ed76ebb36eae610e2769734469849b764f
Jongheon Eom <e4033jh@daum.net>
2023-04-16 18:02:33 +0900

    제네릭 한정적 와일드카드 학습 진행 중

---
+++ src/effective/ch05/item31/Stack.java
@@ -0,0 +1,47 @@
+package effective.ch05.item31;
+
+import java.util.Arrays;
+import java.util.EmptyStackException;
+
+public class Stack<E> {
+    private Object[] elements;
+    private int size = 0;
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+
+    public Stack() {
+        elements = new Object[DEFAULT_INITIAL_CAPACITY];
+    }
+
+    public void push(E e) {
+        ensureCapacity();
+        elements[size++] = e;
+    }
+
+    public E pop() {
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+        E result = (E) elements[--size];
+        elements[size] = null; // 다 쓴 참조 해제
+        return result;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private void ensureCapacity() {
+        if (elements.length == size) {
+            elements = Arrays.copyOf(elements, 2 * size + 1);
+        }
+    }
+
+    /**
+     * 추가되는 메서드
+     */
+    public void pushAll(Iterable<? extends E> src) {
+        for (E e : src) {
+            push(e);
+        }
+    }
+}
